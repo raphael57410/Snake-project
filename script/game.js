131e3d9e9aadf09e80cf22843777496aef3f5db2
TristanSch1 <tristan.sch57@gmail.com>
2022-03-08 15:42:13 +0100

    ✨ Finish player creation

--- script/game.js
+++ script/game.js
@@ -1,1 +1,18 @@
-class Game {}
+class Game extends Player {
+  constructor(numOfPlayer) {
+    this.numOfPlayer = numOfPlayer;
+  }
+
+  displayPlayers() {
+    for (let i = 1; i <= this.numOfPlayer; i++) {
+      document.getElementById("playerBloc").innerHTML += `
+            <div class="bloc_player">
+                <h2>Joueur ${i}</h2>
+                <p></p>
+                <p id="${this.name + this.number}">Position : Case ${
+        this.position
+      }</p>
+            </div>`;
+    }
+  }
+}
--- script/main.js
+++ script/main.js
@@ -1,12 +1,21 @@
 const numberPlayer = document.getElementById("numberPlayer");
 const startButton = document.getElementById("startGame");
 const playerName = document.getElementById("playerName");
-
+const startingPage = document.getElementById("startingPage");
+let playerNumber = 0;
+//Choix du nombre de joueur et génération du formulaire pour les pseudos
 numberPlayer.addEventListener("change", function (event) {
   let innerString = "";
-  for (let i = 1; i <= event.target.value; i++) {
+  playerNumber = event.target.value;
+  for (let i = 1; i <= playerNumber; i++) {
     innerString += `<label for="pName">Nom du joueur ${i} : </label>
         <input type="text" name="pName"/>`;
   }
   playerName.innerHTML = innerString;
+});
+
+//Création de la partie
+
+startButton.addEventListener("click", function () {
+  startingPage.hidden = true;
 });
